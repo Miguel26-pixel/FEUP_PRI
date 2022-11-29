882e66eca011be409c61c1c930aeee8394b8db80
Miguel Amorim <migamorim2012@gmail.com>
2022-11-29 14:04:43 +0000

    Initial frontend

---
+++ Milestone3/my-project/components/Layout.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Head from 'next/head';
+
+export default function Layout({children}) {
+    return (
+        <><Head>
+            <title>SPOTYFIND</title>
+            <link rel="icon" type="image/x-icon" href="/plant.ico"></link>
+        </Head>
+        <div>
+            {children}
+        </div>
+        </>
+    )
+}
+
+
